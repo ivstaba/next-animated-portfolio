b08dce1e98d2bdc162a6fab30411cc5b23d8e915
ivstaba <istaba04@gmail.com>
2024-04-01 14:13:58 +0200

    added basic pages

---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+const Navbar = () => {
+    return (
+        <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+            <div className="">
+                <Link href="/" className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
+                    <span className="text-white mr-1">Lama</span>
+                    <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">.dev</span>
+                </Link>
+            </div>    
+        </div>
+    );
+};
+
+export default Navbar;
